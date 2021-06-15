2fbf466532cba26a530918f1453671c9e5618b5b
lozza <lxw@purelxw.me>
2021-06-15 02:19:31 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import time
 
 os.system('cls & mode 70, 32 & title github profile views booster | by lozza (github.com/qro)')
 user = "https://camo.githubusercontent.com/673e0d6022ecef69601e1c5cf4ec6e271570b43797867108d9dcc74f504fa6b6/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d71726f26636f6c6f723d6c6967687467726579" # put profile here
@@ -9,4 +10,4 @@ pfile = open(p, 'r')
 for txt in pfile:
 	prox = {'http': txt}
 	r = requests.get(user, headers={'User-Agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Mobile Safari/537.36 Edg/88.0.705.63'}, proxies=prox)
-	print(" [>] Profile viewed ")
+	print(time.strftime(" [>] (%H:%M:%S) Request sent"))
